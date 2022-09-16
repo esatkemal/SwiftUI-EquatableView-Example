9f7183de93c9c90fe02f760d4959fa8629d9563e
Esat Kemal Ekren <esat.kemal.ekren@netguru.com>
2022-09-16 10:55:58 +0200

    Initial Commit

---
+++ SwifUI-EquatableView-Example/SwifUI_EquatableView_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifUI_EquatableView_ExampleApp.swift
+//  SwifUI-EquatableView-Example
+//
+//  Created by Kemal Ekren on 16/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwifUI_EquatableView_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwifUI-EquatableView-ExampleUITests/SwifUI_EquatableView_ExampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwifUI_EquatableView_ExampleUITestsLaunchTests.swift
+//  SwifUI-EquatableView-ExampleUITests
+//
+//  Created by Kemal Ekren on 16/09/2022.
+//
+
+import XCTest
+
+final class SwifUI_EquatableView_ExampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
